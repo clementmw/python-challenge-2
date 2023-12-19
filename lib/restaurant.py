4217a066c72902c38e5c4eb347fd2094395245e2
Clement Macharia <clementmacharia62@gmail.com>
2023-12-20 00:19:22 +0300

    added rest

--- lib/restaurant.py
+++ lib/restaurant.py
@@ -2,10 +2,26 @@ class Restaurant:
 
     def __init__ (self,name):
         self._name = name # shows others its a private attribute
+        self.review = [ ] # an empty list
     
     @property
     def _name(self):
         return self._name
+    
+    def add_review(self,review):
+        self.review.append(review)
+        return self.review
+    
+    def get_review (self):
+        return self.review
+    
+    def customers(self):
+        unique_customers = set()  # Using a set to ensure uniqueness
+        for review in self.reviews:
+            unique_customers.add(review.customer)
+        return list(unique_customers)
+    
+    
 
 restrant = Restaurant ("chapo baze")
 print(restrant.name)
--- lib/review.py
+++ lib/review.py
@@ -4,12 +4,18 @@ class Review:
     all_review = []
 
     def __init__ (self,customer,restaurant,rating):
-        self.customer = customer
-        self.restaurant = restaurant
+        self._customer = customer
+        self._restaurant = restaurant
         self.rating = rating
 
     def review_rating (self):
         return self.rating
+    
+    def review_customer (self):
+        return self._customer
+    
+    def review_restaurant (self):
+        return self._restaurant
     
     @classmethod
     def get_all_reviews(cls):
